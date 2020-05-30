8e05701f8e8b9404679292f4f92bdf6d63675957
Arthur Nangai <arthurnangaiarty@yahoo.co.uk>
2020-05-30 16:23:38 +0300

    ch(tests) - Set up CI with travis. (#4)
    - Test get articles.
    - Add travis.yml file.
    
    [Finishes #173093443]

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from articles.models import Article
 
-# Create your tests here.
+
+class ArticleTest(APITestCase):
+
+    def test_get_article(self):
+        article = Article()
+        article.headline = "Getting started"
+        article.summary = "Getting Started"
+        article.image_url = "https://pixabay.com/images/id-1031754/"
+        article.author = "John Doe"
+        article.slug = "getting_started"
+        article.save()
+        url = reverse('articles:index')
+        response = self.client.get(url)
+        articles = response.json()
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(articles[0]['headline'], article.headline)
--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from articles.views import ArticleView
 
+app_name = 'articles'
 urlpatterns = [
     path('', ArticleView.as_view(), name='index'),
 ]
